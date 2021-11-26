b464b38a88e3564641fb8bbe0037bcf73e15502f
Luca <lucagregolini@gmail.com>
2021-11-26 14:17:22 -0300

    sucess js build

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,5 +1,13 @@
 import { createConnection } from "typeorm";
 
-createConnection()
+createConnection({
+  type: "sqlite",
+  database: "./db.sqlite",
+  synchronize: false,
+  logging: false,
+  entities: ["../models/**/*.{ts,js}"],
+  migrations: ["../db/migrations/**/*.{ts,js}"],
+  subscribers: ["../db/subscribers/**/*.{ts,js}"],
+})
   .then(() => console.log("📖 Successfully connected with database"))
   .catch((error) => console.log("😧 error connected with database: " + error));
