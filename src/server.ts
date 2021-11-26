5feb5ead722e4badf1950d411740a67eced7cf3f
Luca <lucagregolini@gmail.com>
2021-11-26 15:06:52 -0300

    error handler fixed

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,6 @@ import app from "./app";
 import "./db/connect";
 
 const port = process.env.PORT || 80;
+const url = process.env.BASE_URL || "localhost";
 
-app.listen(port, () => console.log(`🔥 Server started at http://localhost:${port}`));
+app.listen(port, () => console.log(`🔥 Server started at http://${url}:${port}`));
